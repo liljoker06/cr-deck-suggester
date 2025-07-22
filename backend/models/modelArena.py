3cc1ccdccfe5f4afa38822807bcb4866a909a1e0
liljoker06 <lahrimialadin@gmail.com>
2025-07-22 23:19:10 +0200

    Ajout de commentaires pour clarifier les attributs de la classe Arena

--- backend/models/modelArena.py
+++ backend/models/modelArena.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel
 
 class Arena(BaseModel):
-    number: int
-    name: str
+    number: int # Numéro de l'arène
+    name: str   # Nom de l'arène
     image: str  # URL de l’image
     min_trophies: int
